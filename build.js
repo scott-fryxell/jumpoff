b4f1323582bfafb47b4a6ac105ea91a3fe1b6d94
Scott Fryxell <sfryxell@me.com>
2019-07-08 23:03:47 -0700

    Achieve feature parity with harp

--- build.js
+++ build.js
@@ -1,16 +1,29 @@
-var Metalsmith = require('metalsmith'),
+let Metalsmith = require('metalsmith'),
     markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates');
+    layouts    = require('metalsmith-layouts'),
+    stylus     = require('metalsmith-stylus')
+    serve      = require('metalsmith-serve'),
+    watch      = require('metalsmith-watch');
 
 Metalsmith(__dirname)
-  .source('./src')
-  .destination('./build')
-  .use(markdown())
-  .use(templates('handlebars'))
-  .build(err => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('Site build complete!')
-    }
-  })
+.source('./src')
+.destination('./build')
+.use(stylus())
+.use(markdown())
+.use(layouts({
+  default: "default.hbs",
+  directory: "src/layouts",
+  pattern: "**/*.html"
+}))
+.use(serve({
+  port: 9000,
+  verbose: true
+}))
+.use(watch({
+  pattern: '**/*',
+  livereload: true
+}))
+.build(err => {
+  if (err) throw err;
+  console.log('Site build complete!');
+})
